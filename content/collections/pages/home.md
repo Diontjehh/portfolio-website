4b2678c6de657d7385010814159dfd465757e68a
dion boerrigter <dion@cube.nl>
2025-03-09 21:59:30 +0100

    page header

--- content/collections/pages/home.md
+++ content/collections/pages/home.md
@@ -2,8 +2,34 @@
 id: fd103c7d-a8a3-4949-a10c-6fed84b6f907
 blueprint: page
 title: Home
-header_title: Test
+header_title: 'Ik ben een software Developer'
 header_subtitle: test
 updated_by: 1
-updated_at: 1734096243
+updated_at: 1741553720
+header_image: avatar.png
+header_name: 'Dion Boerrigter'
+header_description: 'Hier komt wat informatie over mijzelf dohaefiae fhe aifehius hf hishuif shiufhiusrhifrhsigshgiurshiguhrsiughsirhgishgisrhrgis gs ihrhgigihrsighs i'
+header_buttons:
+  -
+    id: m822lyvf
+    button: test1
+    type: buttons
+    enabled: true
+    text_field: test10
+    link: test1
+    title: test1
+  -
+    id: m822m84o
+    button: test2
+    type: buttons
+    enabled: true
+    text_field: test20
+    link: test2
+    title: test2
+dynamic_components:
+  -
+    id: m822uvjj
+    type: test
+    enabled: true
+header_avatar: avatar.png
 ---
--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,3 +1,29 @@
-<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-    <img src="{{ asset($page->header_image) ?? null }}" alt="{{ $page->header_image_alt ?? null }}" class="aspect-square rounded-lg overflow-clip block w-4/5 h-4/5 object-cover" />
+<div class="flex items-center justify-center min-h-[70vh] pt-16">
+    <div class="text-center max-w-2xl">
+        <div class="flex justify-center">
+            @if ($page->header_image)
+                <img src="{{ asset($page->header_image) }}" alt="{{ $page->header_image->alt ?? '' }}" class="rounded-full w-44 h-44" />
+            @endif
+        </div>
+
+        <h3 class="text-lg font-bold mt-6">
+            {{ $page->header_name }}
+        </h3>
+
+        <h1 class="text-4xl sm:text-5xl font-bold mt-2">
+            {{ $page->header_title }} <br>
+        </h1>
+
+        <p class="text-gray-400 mt-4 text-lg max-w-lg mx-auto">
+            {{ $page->header_description }}
+        </p>        
+
+        <div class="mt-6 flex justify-center gap-4">
+            @foreach ($page->header_buttons as $button)
+                <a href="{{ $button->link }}" class="px-6 py-3 rounded-full font-semibold shadow-md">
+                    {{ $button->title }}
+                </a>
+            @endforeach
+        </div>
+    </div>
 </div>
